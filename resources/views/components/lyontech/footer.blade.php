ca9836c2d4c781aa156fa5a8bad471ef4dfaa8b0
ElRodriguez <elrodriguez2423@github.com>
2024-11-08 20:07:33 -0500

    comentado libro

--- resources/views/components/lyontech/footer.blade.php
+++ resources/views/components/lyontech/footer.blade.php
@@ -6,10 +6,10 @@
             </p>
         </div>
         <div class="col-2">
-            <a href="{{ route('lyon_librodereclamos') }}" target="_blank" class="text-center mt-4">
+            {{-- <a href="{{ route('lyon_librodereclamos') }}" target="_blank" class="text-center mt-4">
                 <span class="text-white mt-4">libro de reclamaciones</span>
                 <img src="/img/libro-escribiendo.svg" style="width: 120px;height:120px" />
-            </a>
+            </a> --}}
         </div>
     </div>
 
