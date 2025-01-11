30caf834177f8d5c48b48ac3c2e6643f4768b51d
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2025-01-11 02:38:05 -0500

    footer nuevo

--- resources/views/components/lyontech/footer.blade.php
+++ resources/views/components/lyontech/footer.blade.php
@@ -1,5 +1,5 @@
 <section style="width: 100%">
-    <div class="row justify-content-between">
+    {{-- <div class="row justify-content-between">
         <div class="col-4 align-middle">
             <p class="text-white mt-4">
                 &copy; 2024 todos los derechos reservados
@@ -18,6 +18,154 @@
                 </a>
             </p>
         </div>
-    </div>
+    </div> --}}
+
+    <footer class="bg-black text-center text-lg-start text-white">
+        <!-- Grid container -->
+        <div class="container p-4">
+          <!--Grid row-->
+          <div class="row my-4">
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+
+              <div class="rounded-square bg-white shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 140px; height: 150px;">
+                <img src="{{ ENV('APP_URL') }}/theme-lyontech/images/icon.jpg" height="150" alt=""
+                     loading="lazy" />
+              </div>LYONTEACH
+
+              <p class="text-center">TU INVESTIGACIÓN MAS FACIL, AHORA.</p>
+
+              <ul class="list-unstyled d-flex flex-row justify-content-center">
+                <li>
+                  <a target="_blank" class="text-white px-2" href="https://www.facebook.com/lyonteach0216">
+                    <i class="fab fa-facebook-square"></i>
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" class="text-white px-2" href="https://www.instagram.com/lyonteach">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                    <a class="text-white ps-2" href="#!">
+                      <i class="fab fa-youtube"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a target="_blank" class="text-white ps-2" href="https://www.tiktok.com/@lyonteach0216">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"/></svg>
+                    </a>
+                  </li>
+              </ul>
+
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4 text-left">HERRAMIENTAS</h5>
+
+              <ul class="list-unstyled text-left">
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Consultas IA.</a>
+                </li>
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Hoja de Trabajo.</a>
+                </li>
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Mis investigaciones.</a>
+                </li>
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Cursos.</a>
+                </li>
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Chats.</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4 text-left">Nosotros</h5>
+
+              <ul class="list-unstyled text-left">
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Políticas de privacidad</a>
+                </li>
+                <li class="mb-2">
+                  <a target="_blank" href="{{ route('lyon_librodereclamos') }}" class="text-white"></i>Libro de reclamaciones</a>
+                </li>
+                <li class="mb-2">
+                  <a target="_blank" href="{{ route('terms_conditions') }}" class="text-white"></i>Términos y condiciones</a>
+                </li>
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Membresias</a>
+                </li>
+                <li class="mb-2">
+                  <a href="#!" class="text-white"></i>Presentación</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4 text-left">Contacto</h5>
+
+              <ul class="list-unstyled text-left">
+                <li class="mb-2">
+                  <p><i class="fas fa-map-marker-alt pe-2"></i>Chimbote, 57 Street, Perú</p>
+                </li>
+                <li class="mb-2">
+                  <p><i class="fas fa-phone pe-2"></i>+ 51 555 555 555</p>
+                </li>
+                <li class="mb-2">
+                    <p><i class="fas fa-envelope pe-2 mb-0"></i>contact@example.com</p>
+                  </li>
+                  <li class="mb-2">
+                    <p><div id="whatsapp">
+                        <a target="_blank" href="https://api.whatsapp.com/send?phone=51922477831&amp;text=Hola&nbsp;Lyonteach!&nbsp;me&nbsp;pueden&nbsp;ayudar con mi investigación?" class="wtsapp" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="text-white fab fa-whatsapp">Whatsapp</i>
+                        </a>
+                    </div></p>
+                  </li>
+
+              </ul>
+            </div>
+            <!--Grid column-->
+
+             <!--Grid column-->
+             <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-4 text-left">Acceso</h5>
+
+                <ul class="list-unstyled text-left">
+                    <li class="mb-2">
+                      <a href="{{ route('ly-login') }}" class="text-white"></i>Iniciar Sesión</a>
+                    </li>
+                    <li class="mb-2">
+                      <a href="{{ route('register') }}" class="text-white"></i>Registrarse</a>
+                    </li>
+                    <li class="mb-2">
+                      <a href="{{ route('register') }}" class="text-white"></i>Empezar gratis</a>
+                    </li>
+                    <li class="mb-2">
+                      <a href="{{ route('password.request') }}" class="text-white"></i>Olvido su contraseña</a>
+                    </li>
+                  </ul>
+              </div>
+              <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+          RUC: 20612240541 - Lyonteach Educational Research  Technology S.A.C.
+          <a class="text-white" href="{{ ENV("APP_URL") }}">©Lyonteach 2025</a>
+        </div>
+        <!-- Copyright -->
+      </footer>
 
 </section>
