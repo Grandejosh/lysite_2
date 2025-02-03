775002836060f3612cfc0ea7fa96657dde53ff4f
Yisus Web <connexion.jesus@gmail.com>
2025-02-03 12:15:58 -0500

    ghgh

--- resources/views/auth/ly-register.blade.php
+++ resources/views/auth/ly-register.blade.php
@@ -12,7 +12,7 @@
                     <div class="container-fluid ">
                         <div class="col-lg-12 col-md-12 col-sm-10">
                             <div class="card card-transparent text-login">
-                                <h5>BIENVENIDOS</h5>
+                                <h5>BIENVENIDOS</h5> Mas prueba
                                 <p class=" font-size-16pt " style="letter-spacing: -1px; word-spacing: -1px; line-height: 1;">
                                     a la prueba gratuita
                                 </p>
