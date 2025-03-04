7a2a66f242742413683131a1e29708a2ca58914e
Grandejosh <grande_josh@odontologos.com>
2025-03-03 20:31:18 -0500

    s
    s

--- app/Http/Livewire/HelpGpt/LyBoxGpt.php
+++ app/Http/Livewire/HelpGpt/LyBoxGpt.php
@@ -240,7 +240,7 @@ class LyBoxGpt extends Component
             }
             //dd($this->message);
             if ($messages != false && $break == false) {
-                $messages = $messages->json();
+
                 $messages = $messages['response'];
                 try {
                     $resultado = $messages;   //la respuesta final
