8c3e9b500f35282aed69a66c37ee1a5b93f3dfcd
Grandejosh <grande_josh@odontologos.com>
2025-03-03 20:18:01 -0500

    s
    s

--- app/Http/Livewire/HelpGpt/LyBoxGpt.php
+++ app/Http/Livewire/HelpGpt/LyBoxGpt.php
@@ -474,7 +474,7 @@ class LyBoxGpt extends Component
                     $user_id = Auth::user()->id;
                     $message = $msg;
                     $archivo = $archivo;
-
+dd($user_id, $message, $archivo);
                     // URL del servidor Flask
                     $url = 'http://127.0.0.1:5000/assistant_ai';
 
