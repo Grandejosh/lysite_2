fd9aa7c2de31039695403f85ba800abbf48d15a8
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2025-03-07 18:33:07 -0500

    f
    f

--- app/Http/Livewire/HelpGpt/LyBoxGpt.php
+++ app/Http/Livewire/HelpGpt/LyBoxGpt.php
@@ -288,13 +288,13 @@ class LyBoxGpt extends Component
                 switch ($this->prompt) {
 
                     case  0:
-                        $consulta = "Parafraséame este texto en español como si fueras un experto en investigación, pero agrega cierta inconsistencia y errores gramaticales como lo haría un humano intenta no ser predecible: ";
+                        $consulta = "Parafraséame este texto en español como si fueras un experto en investigación, pero agrega cierta inconsistencia y errores gramaticales como lo haría un humano eso si siempre con palabras formales: ";
                         break;
                     case  1:
-                        $consulta = "Parafraséame este texto en español con el objetivo de reducir el mayor grado de similitud, pero agrega cierta inconsistencia y errores gramaticales como lo haría un humano intenta no ser predecible: ";
+                        $consulta = "Parafraséame este texto en español con el objetivo de reducir el mayor grado de similitud, pero agrega cierta inconsistencia y errores gramaticales como lo haría un humano, eso si siempre con palabras formales: ";
                         break;
                     case  2:
-                        $consulta = "Parafraséame este texto en español logrando humanizarlo por completo, asimismo, reduciendo el mayor grado de similitud posible, pero agrega cierta inconsistencia y errores gramaticales como lo haría un humano intenta no ser predecible: ";
+                        $consulta = "Parafraséame este texto en español logrando humanizarlo por completo, asimismo, reduciendo el mayor grado de similitud posible, pero agrega cierta inconsistencia y errores gramaticales como lo haría un humano intenta no ser predecible eso si siempre con palabras formales: ";
                         break;
                 }
 
